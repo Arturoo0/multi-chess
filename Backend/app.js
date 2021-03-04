1b2b1467e9c550b5817d5baadbbfa625b3084f52
Arturoo0 <arturoportelles@gmail.com>
2021-03-04 00:07:57 -0500

    msg logger

--- Backend/app.js
+++ Backend/app.js
@@ -1,2 +1,13 @@
 
-./node_modules
+const express = require('express');
+const app = express();
+const { performance } = require('perf_hooks');
+
+const msgLog = (logMsg, time) => {console.log(logMsg, time)};
+
+app.get('/', function (req, res) {
+  res.send('Server is running.');
+  
+})
+
+app.listen(3000)
