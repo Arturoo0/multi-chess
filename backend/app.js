02b6c373fb4bb0a536454142357a89bd5db3559a
Arturoo0 <arturoportelles@gmail.com>
2021-03-04 20:02:38 -0500

    join room logic

--- backend/app.js
+++ backend/app.js
@@ -15,10 +15,23 @@ app.get('/', function (req, res) {
   res.send('Server is running.');
 })
 
+let rooms = {};
 io.on('connection', (socket) => {
   console.log('Connected to by id : %s', socket.id);
   socket.on('testCom', (arg) => {
     console.log(arg);
+  });
+
+  socket.on('joinRoom', () => {
+    if (!(socket.id in rooms)){
+      rooms[socket.id] = {currentlyConnected : 1};
+      socket.join(socket.id);
+    } else if (rooms[socket.id] < 2){
+      socket.join(socket.id);
+      rooms[socket.id].currentlyConnected += 1; 
+    }
+    console.log(rooms);
+    socket.emit('connectedToRoom', rooms[socket.id].currentlyConnected);
   });
 });
 
--- frontend/src/pages/PlayMatch.js
+++ frontend/src/pages/PlayMatch.js
@@ -4,14 +4,21 @@ import { Board } from '../components';
 import socketIOClient from "socket.io-client";
 
 const PlayMatch = () => {
+    const [connectedUsers, setUserCount] = useState(0);
     useEffect(() => {
         const SERVER = "http://localhost:3000/";
         const socket = socketIOClient(SERVER);
-        socket.emit('testCom', 'testMsg');
+        // socket.emit('testCom', 'testMsg');
+        socket.emit('joinRoom');
+        socket.on('connectedToRoom', (numberOfMembers) => {
+            console.log('returnedValue', numberOfMembers)
+            setUserCount(numberOfMembers);
+        })
     }, []);
 
     return (
         <div>
+            <p>Current room members - {connectedUsers}</p>
             <Board />
         </div>
     );
