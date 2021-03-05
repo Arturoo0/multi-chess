87d23ec1c8a57a30260f4eada96c94575ae1ef1b
Arturoo0 <arturoportelles@gmail.com>
2021-03-05 15:25:57 -0500

    board component removal

--- frontend/src/pages/PlayMatch.js
+++ frontend/src/pages/PlayMatch.js
@@ -1,6 +1,5 @@
 
 import React, { useState, useEffect } from "react";
-import { Board } from '../components';
 import socketIOClient from "socket.io-client";
 import Chessboard from "chessboardjsx";
 
