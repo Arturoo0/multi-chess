8a2f5ef6c7b2385d170d496f0e572c866e38227b
Arturoo0 <arturoportelles@gmail.com>
2021-04-04 16:26:23 -0400

    hook into game lobby

--- frontend/src/components/JoinCard/JoinCard.js
+++ frontend/src/components/JoinCard/JoinCard.js
@@ -1,5 +1,7 @@
 
-import { React, useRef } from 'react';
+import { React, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button,
@@ -7,13 +9,17 @@ import {
 } from 'reactstrap';
 import './JoinCard.css';
 
-const JoinCard = () => {
-    const inputLobbyRef = useRef(null);
+const JoinCard = (props) => {
+    const [joinRoomState, setJoinRoomState] = useState(null);
+    const history = useHistory();
     const handleJoinSubmit = () => {
-        // check backend for existing lobby
-        // lobby does not exist display alert message
-        window.alert('No such lobby exists.');
+        axios.get(`http://localhost:3000/doesRoomExist?roomID=${joinRoomState}`)
+        .then(res => {
+            if (res.data === 0) window.alert('No such lobby exists.');
+            else history.push(`playMatch?match=${joinRoomState}`)
+        })
     }
+    
     return (
         <div>
             <Card>
@@ -23,7 +29,10 @@ const JoinCard = () => {
                     <CardText>Paste the given URL into the browser or provided box.</CardText>
                     <Form inline id='form-submit-container'>
                         <FormGroup className="mb-2 me-sm-2 mb-sm-0">
-                            <input ref={inputLobbyRef} placeholder="Lobby URL"></input>
+                            <input 
+                                placeholder="Lobby URL"
+                                onChange={event => setJoinRoomState(event.target.value)}
+                            ></input>
                         </FormGroup>
                         <Button onClick={handleJoinSubmit}>Submit</Button>
                     </Form>
