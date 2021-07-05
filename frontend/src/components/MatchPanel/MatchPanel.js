e964f0a2b91183ed4d44fd0b35dd05d02a17fc75
Arturo Portelles <arty@Arturos-MacBook-Pro.local>
2021-07-05 13:16:10 -0400

    Imports / checkpoint

--- frontend/src/components/MatchPanel/MatchPanel.js
+++ frontend/src/components/MatchPanel/MatchPanel.js
@@ -1,9 +1,13 @@
 
 import React, { useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 
 const MatchPanel = (props) => {
     return (
         <div style={{backgroundColor : props.config.color}}>
+            <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+            </Spinner>
             Connected Users : {props.config.connectedPlayers}
         </div> 
     );
